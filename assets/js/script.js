b2d879c532838a124915e3b6de8badee8c8c7ed4
jeniferwj <81196036+jeniferwj@users.noreply.github.com>
2026-03-30 17:45:08 -0300

    h

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,11 @@ function mensagem() {
     alert("Inscrição realizada com sucesso.");
 }
 
-// DATA DO EVENTO (ano, mês-1, dia, hora, minuto)
-const dataEvento = new Date(2026, 3, 30, 20, 0, 0); 
-// Abril = 3 (começa do 0)
+const dataEvento = new Date(2026, 7, 23, 19, 0, 0); 
 
 function atualizarContador() {
   const agora = new Date();
   const diferenca = dataEvento - agora;
-
-  if (diferenca <= 0) {
-    document.getElementById("tempo").innerText = "O evento começou! 🚀";
-    return;
-  }
 
   const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
   const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
@@ -24,8 +17,6 @@ function atualizarContador() {
     `${dias} dias, ${horas}h ${minutos}m ${segundos}s`;
 }
 
-// Atualiza a cada 1 segundo
 setInterval(atualizarContador, 1000);
 
-// Executa na hora que carrega
 atualizarContador();
